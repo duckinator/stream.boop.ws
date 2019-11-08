263459ca073884a7b9320667ae37d005c64c6f05
Ellen Marie Dash <me@duckie.co>
2019-11-08 18:56:53 -0500

    allow only whitelisted IPs to publish

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ RUN apt-get update && apt-get -y install nginx libnginx-mod-rtmp && \
 COPY files/etc/ /etc/
 COPY files/frontend/ /frontend/
 
+ARG ALLOWED_IP=127.0.0.1
+
+RUN sed -i 's/#allow publish ALLOWED_IP;/allow publish '"$ALLOWED_IP"';/' /etc/nginx/nginx.conf
+
 EXPOSE 80
 EXPOSE 443
 EXPOSE 1935
--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo "Building."
-docker build -t stream . || exit $?
+
+if [ "$1" == "--allowed-ip" ]; then
+  ALLOWED_IP="$2"
+fi
+
+if [ -n "$ALLOWED_IP" ]; then
+  docker build --build-arg "ALLOWED_IP=$ALLOWED_IP"  -t stream . || exit $?
+else
+  docker build -t stream . || exit $?
+fi
 
 echo "Running."
-docker run --rm -it -p 80:80 -p 1935:1935 stream
+docker run --rm -it --entrypoint /bin/bash -p 80:80 -p 1935:1935 stream
