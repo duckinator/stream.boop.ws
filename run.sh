4017c7c4d4e167d43acb080d76668667956711df
Ellen Marie Dash <me@duckie.co>
2020-09-27 22:55:39 -0400

    Make run.sh a bit more robust.

--- run.sh
+++ run.sh
@@ -2,15 +2,23 @@
 
 echo "Building."
 
+if [ -z "$DOCKER" ]; then
+    DOCKER=docker
+fi
+
+if [ -z "$STREAM_PORT" ]; then
+    STREAM_PORT=80
+fi
+
 if [ "$1" == "--allowed-ip" ]; then
   ALLOWED_IP="$2"
 fi
 
 if [ -n "$ALLOWED_IP" ]; then
-  docker build --build-arg "ALLOWED_IP=$ALLOWED_IP"  -t stream . || exit $?
+  $DOCKER build --build-arg "ALLOWED_IP=$ALLOWED_IP"  -t stream . || exit $?
 else
-  docker build -t stream . || exit $?
+  $DOCKER build -t stream . || exit $?
 fi
 
 echo "Running."
-docker run --rm -it -p 80:80 -p 1935:1935 stream
+$DOCKER run --rm -it -p $STREAM_PORT:80 -p 1935:1935 stream
